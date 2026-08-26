657b3f6f20d0cb09e2a5010ced7691e46fffde6f
muluken <mulukenbekalu529@gmail.com>
2026-08-26 22:18:41 +0300

    Allow React app to navigate between pages and URLs

--- netflix-clone/src/main.jsx
+++ netflix-clone/src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
@@ -6,9 +5,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* BrowserRouter allows us to use routing and navigate between different pages. */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
-
