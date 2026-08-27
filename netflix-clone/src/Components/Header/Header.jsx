33f169eb8f1d6408dcc8ebb93f6beb9389e34edd
Rahel-zekiros <raki52360@gmail.com>
2026-08-27 12:31:55 +0300

    Update header

--- netflix-clone/src/Components/Header/Header.jsx
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -35,8 +35,10 @@ function Header() {
       <div className={style.headerLeft}>
 
         {/* NETFLIX LOGO */}
-        <div className={style.logo}>NETFLIX</div>
-
+        <div className={style.logo}>
+          <span className={style.logoIcon}>▶</span>
+       <span className={style.logoText}>Flixora</span>
+      </div>
         {/* NAVIGATION */}
         <nav className={style.navMenu}>
           <Link to="" className={style.navLink}>
